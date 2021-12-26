2110e35f0e0451be2caa582f60fa2f24ea1badde
Rafael Nagel <rafael.gustavo.nagel@gmail.com>
2021-12-26 14:31:42 -0300

    ringbuffer class

---
+++ include/ring_buffer.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <types_local.h>
+
+template <typename T, size_t S> class RingBuffer {
+  private:
+    std::array<T, S> _buffer;
+    size_t _head = 0; // write index
+    size_t _tail = 0; // read index
+    size_t _toread = 0;
+
+    void _increment_head() {
+        _head++;
+        if (_head == S)
+            _head = 0;
+    }
+
+    void _increment_tail() {
+        _tail++;
+        if (_tail == S)
+            _tail = 0;
+    }
+
+  public:
+    inline void write(const T data) {
+        _buffer.at(_head) = data;
+        if (_head == _tail) {
+
+            if (_toread == 0) {
+                _increment_head();
+                _toread++;
+            } else {
+                _increment_head();
+                _increment_tail();
+            }
+
+        } else {
+            _increment_head();
+            _toread++;
+        }
+    }
+
+    inline T read(void) {
+        T retval = _buffer.at(_tail);
+
+        if (_toread >= 1) {
+            _toread--;
+            _increment_tail();
+        }
+
+        return retval;
+    }
+
+    size_t toread() { return _toread; }
+};
+
---
+++ tests/ring_buffer.cpp
@@ -0,0 +1,56 @@
+#include "doctest/doctest.h"
+
+#include <ring_buffer.h>
+
+TEST_CASE("Ring Buffer") {
+    RingBuffer<byte_t, 3> rb;
+    CHECK(rb.toread() == 0);
+    rb.write(0xFF);
+    CHECK(rb.toread() == 1);
+    CHECK(rb.read() == 0xFF);
+    CHECK(rb.toread() == 0);
+
+    rb.write(0x11);
+    rb.write(0x22);
+    rb.write(0x33);
+    CHECK(rb.toread() == 3);
+    rb.write(0x44);
+    CHECK(rb.toread() == 3);
+    rb.write(0x55);
+    CHECK(rb.toread() == 3);
+    rb.write(0x66);
+    CHECK(rb.toread() == 3);
+
+    // only the last 3 elements must be available in the ringbuffer
+    CHECK(0x44 == rb.read());
+    CHECK(rb.toread() == 2);
+    CHECK(0x55 == rb.read());
+    CHECK(rb.toread() == 1);
+    CHECK(0x66 == rb.read());
+    CHECK(rb.toread() == 0);
+
+    // if ringbuffer is empty, a read operation returns the first element
+    // amongst the last 3 ones.
+
+    CHECK(0x44 == rb.read());
+    CHECK(0x44 == rb.read());
+    CHECK(0x44 == rb.read());
+    CHECK(0x44 == rb.read());
+    CHECK(rb.toread() == 0);
+
+    // writing after all elements were read
+    rb.write(0x77);
+    CHECK(1 == rb.toread());
+    CHECK(0x77 == rb.read());
+    CHECK(0 == rb.toread());
+
+    rb.write(0x88);
+    rb.write(0x99);
+    rb.write(0xAA);
+    CHECK(3 == rb.toread());
+    CHECK(0x88 == rb.read());
+    CHECK(0x99 == rb.read());
+    CHECK(0xAA == rb.read());
+
+}
+
