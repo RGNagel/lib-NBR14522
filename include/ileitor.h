9a9ac97100428eaebfb911d65613c098a971caac
Rafael Nagel <rafael.gustavo.nagel@gmail.com>
2022-01-02 22:32:44 -0300

    esboço inicial e ideia das classes leitor, porta.

--- include/ileitor.h
+++ include/ileitor.h
@@ -5,17 +5,10 @@
 namespace NBR14522 {
 
 class ILeitor {
-    public:
-
-    enum status {
-        SUCESSO = 0,
-        DESCONECTADO,
-        CRC_ERR,
-        TIMED_OUT,
-        MEMORY_OUT
-    };
+  public:
+    enum status { SUCESSO = 0, DESCONECTADO, CRC_ERR, TIMED_OUT, MEMORY_OUT };
 
     virtual void tx(comando_t comando) = 0;
 };
 
-} // NBR14522
+} // namespace NBR14522
--- include/iporta.h
+++ include/iporta.h
@@ -1,13 +1,19 @@
 #pragma once
 
-#include <stdio.h>
-
 #include <types_local.h>
 
 class IPorta {
-    public:
-    virtual size_t write(const byte_t *data, const size_t data_sz) = 0;
-    virtual size_t read(byte_t *data, const size_t max_data_sz);
-    virtual size_t available() = 0;
-};
+  public:
+    size_t write(const byte_t* data, const size_t data_sz) {
+        return _write(data, data_sz);
+    }
+    size_t read(byte_t* data, const size_t max_data_sz) {
+        return _read(data, max_data_sz);
+    }
 
+    virtual ~IPorta() {}
+
+  protected:
+    virtual size_t _write(const byte_t* data, const size_t data_sz) = 0;
+    virtual size_t _read(byte_t* data, const size_t max_data_sz) = 0;
+};
