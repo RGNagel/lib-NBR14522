490b79110f849e3e3550da62b19e28ba9aff8c1c
Rafael G. Nagel <rafael.nagel@khomp.com>
2022-01-05 19:55:56 -0300

    task scheduler - remove Task && *milliseconds

--- include/task_scheduler.h
+++ include/task_scheduler.h
@@ -3,11 +3,11 @@
 #include <functional>
 #include <map>
 typedef uint32_t tick_t;
-typedef uint32_t miliseconds_t;
+typedef uint32_t milliseconds_t;
 typedef int task_t;
 
-inline tick_t miliseconds2ticks(const miliseconds_t ms,
-                         const miliseconds_t tickPeriod) {
+inline tick_t miliseconds2ticks(const milliseconds_t ms,
+                         const milliseconds_t tickPeriod) {
     if (ms == 0 || tickPeriod == 0 || ms < tickPeriod)
         return 0;
 
@@ -22,47 +22,24 @@ inline tick_t miliseconds2ticks(const miliseconds_t ms,
 }
 
 class TaskScheduler {
-  public:
-    class Task {
-      private:
-        std::function<void()> _callback;
-        task_t _id;
-
-      public:
-        Task(std::function<void()> callback) : _callback(callback) {
-            static task_t id = 0;
-            _id = id++;
-        }
-
-        void callback() {
-            _callback();
-        }
-
-        task_t id() {
-            return _id;
-        }
-    };
-
   private:
 
     tick_t _counter;
-    miliseconds_t _tickPeriod;
-    std::multimap<tick_t, Task> _tasks;
+    milliseconds_t _tickPeriod;
+    std::multimap<tick_t, std::function<void()>> _tasks;
+    bool _stop = false;
 
   public:
 
-    TaskScheduler() : _counter(0) {}
+    TaskScheduler(milliseconds_t tickPeriod = 10) : _counter(0), _tickPeriod(tickPeriod) {}
 
     void addTask(std::function<void()> callback,
-                   miliseconds_t waitAtLeast = 0) {
-
-        Task task(callback);
-        // _tasks.insert(task);
+                   milliseconds_t waitAtLeast = 0) {
         tick_t key = _counter + miliseconds2ticks(waitAtLeast, _tickPeriod);
-        _tasks.insert({key, task});
+        _tasks.insert({key, callback});
     }
 
-    void [[noreturn]] run() {
+    [[noreturn]] void run() {
         while (1) {
             
             if (_tasks.size() <= 0) {
@@ -71,12 +48,12 @@ class TaskScheduler {
             }
 
             for (auto i = _tasks.find(_counter); i->first == _counter; i++) {
-                i->second.callback();
+                i->second();
             }
 
-
             _counter++;
-            // timer.waitMiliseconds(10);
+            std::this_thread::sleep_for(std::chrono::milliseconds(_tickPeriod));
+
         }
     }
 };
