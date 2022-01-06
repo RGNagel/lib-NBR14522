5c6f31d2109bc11464ad29aada36c1d48e2a765d
Rafael G. Nagel <rafael.nagel@khomp.com>
2022-01-06 19:14:08 -0300

    fixup! [WIP] medidor simulado com task scheduler

--- tests/medidor_simulado.cpp
+++ tests/medidor_simulado.cpp
@@ -58,7 +58,7 @@ template <size_t readBufferLen> class MedidorSimulado : public IPorta {
             rb.read();
     }
 
-    bool _comandoRecebido() {
+    void _comandoRecebido() {
 
         uint16_t crcReceived = NBR14522::getCRC(_comando);
         uint16_t crcCalculated =
@@ -83,7 +83,7 @@ template <size_t readBufferLen> class MedidorSimulado : public IPorta {
             }
         }
         // verifica se o comando recebido existe e/ou é valido
-        else if (!NBR14522::isValidCodeCommand(comandoRecebido.at(0))) {
+        else if (!NBR14522::isValidCodeCommand(_comando.at(0))) {
             // a norma não define qual deve ser o comportamento do
             // medidor após receber um comando inválido mas com CRC OK.
             // vamos quebrar a sessão.
@@ -102,7 +102,7 @@ template <size_t readBufferLen> class MedidorSimulado : public IPorta {
     void _readNextPieceOfComando() {
         if (_rb_received.toread() <= 0) {
             // TODO (MAYBE): calcular valor do próximo ENQ levando em consideração o tempo das leituras dos bytes anteriores
-            _tasks.addTask(std:;bind(_ENQ, this), TMINENQ_MSEC);
+            _tasks.addTask(std::bind(_ENQ, this), TMINENQ_MSEC);
             return;
         }
 
@@ -193,126 +193,5 @@ template <size_t readBufferLen> class MedidorSimulado : public IPorta {
         _tasks.run();
     }
 
-    [[noreturn]] void engine() {
-        static enum {
-            CONECTADO,
-            COMANDO_RECEBIDO,
-            TRANSMITE_NAK,
-            RESPOSTA_ENVIADA
-        } engineFSM = CONECTADO;
-
-        size_t previous_toread = 0;
-
-        comando_t comandoRecebido;
-
-        resposta_t resposta;
-
-        int nak_transmitted = 0;
-
-        // GeradorDeRespostas geradorDeResposta;
-
-        _flushReadBuffer();
-
-        while (1) {
-            switch (engineFSM) {
-
-            case COMANDO_RECEBIDO: {
-
-                uint16_t crcReceived = NBR14522::getCRC(comandoRecebido);
-                uint16_t crcCalculated =
-                    CRC16(comandoRecebido.data(), comandoRecebido.size() - 2);
-
-                if (crcReceived != crcCalculated) {
-                    // se CRC com erro, o medidor deve enviar um NAK para o
-                    // leitor e aguardar novo COMANDO. O máximo de NAKs enviados
-                    // para um mesmo comando é 7.
-
-                    _rb_transmitted.write(NBR14522::NAK);
-                    nak_transmitted++;
-
-                    if (nak_transmitted == NBR14522::MAX_BLOCO_NAK) {
-                        nak_transmitted = 0;
-                        // quebra de sequencia, vai imediatamente para o estado
-                        // CONECTADO no qual o medidor envia o ENQ
-                        engineFSM = CONECTADO;
-                        break;
-                    } else {
-                        // TODO: nak_transmitted tem que ser zerado de alguma
-                        // maneira
-
-                        // aguarda tempo máximo de resposta (TMAXRSP) antes de
-                        // ir para CONECTADO, para ver se o leitor envia
-                        // novamente o comando neste periodo. Caso o leitor
-                        // enviar, o estado CONECTADO trata este novo comando
-                        // antes de enviar próximo ENQ (quebra de sequência).
-
-                        // _timer.waitMiliseconds(TMAXRSP_MSEC);
-                        engineFSM = CONECTADO;
-                        break;
-                    }
-                }
-
-                // verifica se o comando recebido existe e/ou é valido
-                if (!NBR14522::isValidCodeCommand(comandoRecebido.at(0))) {
-                    // a norma não define qual deve ser o comportamento do
-                    // medidor após receber um comando inválido mas com CRC OK.
-                    // vamos quebrar a sessão.
-
-                    engineFSM = CONECTADO;
-                    break;
-                }
-
-                // gera a resposta para o comando solicitado
-
-                // geradorDeResposta.set(comandoRecebido);
-
-                // resposta = geradorDeResposta.getNextResposta();
-
-                for (size_t i = 0; i < resposta.size(); i++)
-                    _rb_transmitted.write(resposta.at(i));
-
-                _flushReadBuffer();
-
-                engineFSM = RESPOSTA_ENVIADA;
-            } break;
-            case TRANSMITE_NAK: {
-                _rb_transmitted.write(NBR14522::NAK);
-                nak_transmitted++;
-
-                if (nak_transmitted == NBR14522::MAX_BLOCO_NAK) {
-                    nak_transmitted = 0;
-
-                    // quebra de sequencia, vai imediatamente para o estado
-                    // CONECTADO no qual o medidor envia o ENQ
-                    engineFSM = CONECTADO;
-                    break;
-                }
-
-            } break;
-            case RESPOSTA_ENVIADA: {
-                static uint32_t counter = 0;
-
-                // somente um caracter de sinalização é esperado pelo
-                // medidor
-                if (_rb_received.toread() > 1) {
-                    // quebra de
-                    counter = 0;
-                    engineFSM = CONECTADO;
-                    break;
-                } else if (_rb_received.toread() > 1)
-
-                    // fica aguardando resposta do leitor até estourar o tempo
-                    // TMAXRSP_MSEC
-                    if (counter == 10) {
-                        // tempo de resposta estourado
-                        counter = 0;
-                        engineFSM = CONECTADO;
-                        break;
-                    }
-                // _timer.waitMiliseconds(TMAXRSP_MSEC/10);
-                counter++;
-            } break;
-            }
-        }
-    }
 };
+
