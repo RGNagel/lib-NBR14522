6422575e5ee3690abd89c475ce1af530ecb4ec54
Rafael Nagel <rafael.gustavo.nagel@gmail.com>
2022-02-13 22:49:16 -0300

    leitor - refatoracao pra usar timer ao inves de tasks

--- include/leitor.h
+++ include/leitor.h
@@ -1,280 +1,191 @@
 #pragma once
 
 #include <NBR14522.h>
+#include <log_policy.h>
 #include <task_scheduler.h>
+#include <timer.h>
 
 namespace NBR14522 {
 
 template <typename T> using sptr = std::shared_ptr<T>;
 
-class Leitor {
+using ms = std::chrono::milliseconds;
+
+template <class LogPolicy = LogPolicyStdout> class Leitor {
   public:
     typedef enum {
-        RESPOSTA_RECEBIDA,
-        RESPOSTA_BYTE_RECEBIDO,
-        RESPOSTA_PEDACO_RECEBIDO,
-        DESCONECTADO,
-        SINCRONIZADO,
-        ESPERANDO_RESPOSTA,
-        // erros:
-        NAK_TX_EXCEDIDO,
-        NAK_RX_EXCEDIDO,
-        TEMPO_EXCEDIDO,
-        RESPOSTA_CODIGO_INVALIDO,
-        SINCRONIZACAO_FALHOU,
-        RESPOSTA_CODIGO_DIFERE,
-        RESPOSTA_CODIGO_INVALIDO_ENQ
+        RSP_RECEBIDA,
+
+        ERR_SINC_TEMPO_EXCEDIDO,
+        ERR_SINC_SINALIZADOR,
+
+        ERR_RSP_NAK_EXCEDIDO,
+        ERR_RSP_CODIGO_TEMPO_EXCEDIDO,
+
+        ERR_RSP_NAK_TX_EXCEDIDO,
+        ERR_RSP_TOTAL_TEMPO_EXCEDIDO,
+
+        ERR_RSP_CODIGO_DIFERE,
+        ERR_RSP_CODIGO_INVALIDO_ENQ
     } estado_t;
 
   private:
-    estado_t _estado;
     sptr<IPorta> _porta;
-    TaskScheduler<> _tasks;
-    resposta_t _resposta;
-    std::function<void(resposta_t& rsp)> _callbackDeResposta;
-    comando_t _comando;
-    size_t _respostaIndex;
-    size_t _countTransmittedNAK;
-    size_t _countReceivedNAK;
-    bool _respostaTimedOut = false;
-
-    void _setEstado(estado_t estado) {
-        _estado = estado;
-
-        bool stopTasks = false;
-
-        switch (_estado) {
-        case SINCRONIZADO:
-            _countReceivedNAK = 0;
-            _countTransmittedNAK = 0;
-            _transmiteComando(_comando);
-            break;
-        case RESPOSTA_BYTE_RECEBIDO:
-        case RESPOSTA_PEDACO_RECEBIDO:
-            _tasks.addTask(std::bind(&Leitor::_readNextPieceOfResposta, this),
-                           std::chrono::milliseconds(TMAXCAR_MSEC));
-            break;
-        case RESPOSTA_RECEBIDA:
-            _callbackDeResposta(_resposta);
-
-            if (isComposedCodeCommand(_resposta.at(0))) {
-                // os unicos 3 comandos compostos existentes na norma (0x26,
-                // 0x27 e 0x52) possuem o octeto 006 (5o byte), cujo valor:
-                // 0N -> resposta/bloco intermediário
-                // 1N -> resposta/bloco final
-
-                if (_resposta.at(5) & 0x10) {
-                    // resposta final do comando composto
-                    stopTasks = true;
-                } else {
-                    // resposta intermediária, aguarda próxima resposta
-                    _setEstado(ESPERANDO_RESPOSTA);
-                }
-
-            } else {
-                // comando simples, e unica resposta foi recebida
-                stopTasks = true;
-            }
-            break;
-        case ESPERANDO_RESPOSTA:
-            _tasks.addTask(std::bind(&Leitor::_waitingResposta, this));
-
-            _respostaTimedOut = false;
-            // no data within TMAXRSP or TMAXCAR? then response from meter has
-            // timed out
-            _tasks.addTask(std::bind(&Leitor::_waitingRespostaTimedOut, this),
-                           std::chrono::milliseconds(TMAXRSP_MSEC));
-            break;
-        case NAK_TX_EXCEDIDO:
-        case NAK_RX_EXCEDIDO:
-        case TEMPO_EXCEDIDO:
-        case RESPOSTA_CODIGO_INVALIDO:
-        case SINCRONIZACAO_FALHOU:
-        case RESPOSTA_CODIGO_DIFERE:
-        case RESPOSTA_CODIGO_INVALIDO_ENQ:
-            stopTasks = true;
-            break;
-        default:
-            break;
-        }
-
-        if (stopTasks)
-            _tasks.runStop();
-    }
-    estado_t _getEstado() { return _estado; }
-
-    void _transmiteComando(comando_t& comando) {
-        setCRC(comando, CRC16(comando.data(), comando.size() - 2));
-        _porta->write(comando.data(), comando.size());
-        _setEstado(ESPERANDO_RESPOSTA);
-    }
-
-    void _tentaSincronizarPeriodicamente() {
-        // tentar sincronizar com medidor periodicamente até conseguir
-        // sincronizar
-
-        byte_t data;
-        size_t read = _porta->read(&data, 1);
-        if (read == 1 && data == ENQ) {
-            // após receber o ENQ, temos até TMAXSINC_MSEC para enviar o 1o byte
-            // do comando
-            _setEstado(SINCRONIZADO);
-        } else {
-            _tasks.addTask(
-                std::bind(&Leitor::_tentaSincronizarPeriodicamente, this), 5ms);
-        }
-    }
-
-    void _waitingRespostaTimedOut() {
-        if (_getEstado() == ESPERANDO_RESPOSTA) {
-            _respostaTimedOut = true;
-        }
-    }
-
-    void _waitingResposta() {
-
-        if (_respostaTimedOut) {
-            _setEstado(TEMPO_EXCEDIDO);
-            return;
-        }
-
-        byte_t firstByte;
-        size_t read = _porta->read(&firstByte, 1);
-
-        if (read <= 0) {
-            _tasks.addTask(std::bind(&Leitor::_waitingResposta, this), 10ms);
-            return;
-        }
-
-        // recebeu byte do medidor...
-
-        // TODO: cancel timed out task! Must be implemented in TaskScheduler
-        // class.
-
-        // check if NAK received
-        if (firstByte == NAK) {
-            _countReceivedNAK++;
-            // devemos enviar novamente o comando ao medidor, caso menos de
-            // 7 NAKs recebidos
-            if (_countReceivedNAK >= MAX_BLOCO_NAK) {
-                _setEstado(NAK_RX_EXCEDIDO);
-            } else {
-                // retransmite comando
-                _transmiteComando(_comando);
-            }
-
-            return;
-        }
-
-        // check if ENQ received
-        if (firstByte == ENQ) {
-            _setEstado(RESPOSTA_CODIGO_INVALIDO_ENQ);
-            return;
-        }
-
-        // check if codigo invalido
-        if (!isValidCodeCommand(firstByte)) {
-            printf("codigo invalido: %x\n", firstByte);
-            _setEstado(RESPOSTA_CODIGO_INVALIDO);
-            return;
-        }
-
-        // check if codigo diferente do enviado
-        if (firstByte != _comando.at(0)) {
-            _setEstado(RESPOSTA_CODIGO_DIFERE);
-            return;
-        }
-
-        // primeiro byte recebido OK!
-
-        _resposta.at(0) = firstByte;
-        _respostaIndex = 1;
-        _setEstado(RESPOSTA_BYTE_RECEBIDO);
-    }
-
-    void _readNextPieceOfResposta() {
-        size_t read = _porta->read(&_resposta[_respostaIndex],
-                                   _resposta.size() - _respostaIndex);
-
-        // no data within TMAXRSP or TMAXCAR?
-        if (read <= 0) {
-            // response from meter has timed out
-            _setEstado(TEMPO_EXCEDIDO);
-            return;
-        }
-
-        _respostaIndex += read;
-
-        if (_respostaIndex < _resposta.size()) {
-            // resposta parcial recebida
-            _setEstado(RESPOSTA_PEDACO_RECEBIDO);
-            return;
-        }
-
-        // resposta completa recebida. Trata-a.
-
-        uint16_t crcReceived = getCRC(_resposta);
-        uint16_t crcCalculated = CRC16(_resposta.data(), _resposta.size() - 2);
-
-        if (crcReceived != crcCalculated) {
-            // se CRC com erro, o medidor deve enviar um NAK para o
-            // leitor e aguardar novo COMANDO. O máximo de NAKs
-            // enviados para um mesmo comando é 7.
-
-            if (_countTransmittedNAK >= MAX_BLOCO_NAK) {
-                _setEstado(NAK_TX_EXCEDIDO);
-            } else {
-                byte_t sinalizador = NAK;
-                _porta->write(&sinalizador, 1);
-                _countTransmittedNAK++;
-
-                // aguarda retransmissao da resposta pelo medidor
-                _setEstado(ESPERANDO_RESPOSTA);
-            }
-
-            return;
-        }
-
-        // resposta completa recebida está OK!
-
-        // transmite um ACK para o medidor
-        byte_t sinalizador = ACK;
-        _porta->write(&sinalizador, 1);
-
-        _setEstado(RESPOSTA_RECEBIDA);
-    }
 
   public:
-    Leitor(sptr<IPorta> porta) : _porta(porta) { _setEstado(DESCONECTADO); }
+    Leitor(sptr<IPorta> porta) : _porta(porta) {}
 
-    estado_t tx(const comando_t& comando,
-                std::function<void(const resposta_t& rsp)> cb_rsp_received,
-                std::chrono::milliseconds timeout) {
+    estado_t read(comando_t& comando,
+                  std::function<void(const resposta_t& rsp)> cb_rsp_received,
+                  std::chrono::milliseconds timeout) {
+
+        Timer<> txTimer(timeout);
+
+        resposta_t resposta;
+        size_t respostaIndex = 0;
 
         // discarta todos os bytes até agora da porta
         byte_t data;
         while (_porta->read(&data, 1))
             ;
 
-        _comando = comando;
-        _callbackDeResposta = cb_rsp_received;
-        _setEstado(DESCONECTADO);
-        _tasks.addTask(
-            std::bind(&Leitor::_tentaSincronizarPeriodicamente, this));
+        // aguarda ENQ da porta
+        Timer<> timer;
+        byte_t enq = 0;
+        timer.setTimeout(ms(TMAXENQ_MSEC));
+        while (!timer.timedOut() && !txTimer.timedOut()) {
+            if (_porta->read(&enq, 1) == 1)
+                break;
+        }
 
-        // timeout action
-        _tasks.addTask(
-            [this]() {
-                if (this->_getEstado() == DESCONECTADO) {
-                    this->_setEstado(SINCRONIZACAO_FALHOU);
-                    this->_tasks.runStop();
+        if (txTimer.timedOut() || timer.timedOut())
+            return ERR_SINC_TEMPO_EXCEDIDO;
+
+        if (enq != ENQ)
+            return ERR_SINC_SINALIZADOR;
+
+        // sincronizado
+
+        setCRC(comando, CRC16(comando.data(), comando.size() - 2));
+        _porta->write(comando.data(), comando.size());
+
+        // ---------------------------------
+        // Aguarda primeiro byte da resposta
+        // ---------------------------------
+
+    waitFirstByte:
+        size_t countReceivedNAK = 0;
+
+        // devemos receber o primeiro byte da resposta em até TMAXRSP
+        timer.setTimeout(ms(TMAXRSP_MSEC));
+        while (!timer.timedOut() && !txTimer.timedOut()) {
+            byte_t firstByte = 0;
+            if (_porta->read(&firstByte, 1) == 1) {
+                if (firstByte == NAK) {
+                    countReceivedNAK++;
+                    // devemos enviar novamente o comando ao medidor, caso menos
+                    // de 7 NAKs recebidos
+                    if (countReceivedNAK >= MAX_BLOCO_NAK) {
+                        return ERR_RSP_NAK_EXCEDIDO;
+                    } else {
+                        // retransmite comando
+                        _porta->write(comando.data(), comando.size());
+                        continue;
+                    }
+                } else if (firstByte == ENQ) {
+                    return ERR_RSP_CODIGO_INVALIDO_ENQ;
+                } else if (firstByte != comando.at(0)) {
+                    return ERR_RSP_CODIGO_DIFERE;
                 }
-            },
-            timeout);
 
-        _tasks.run();
+                // primeiro byte recebido OK!
 
-        return _getEstado();
+                resposta.at(0) = firstByte;
+                respostaIndex = 1;
+                break;
+            }
+        }
+
+        if (txTimer.timedOut() || timer.timedOut())
+            return ERR_RSP_CODIGO_TEMPO_EXCEDIDO;
+
+        size_t countTxNAK = 0;
+
+        // ----------------------------------------------------------
+        // Primeiro byte recebido. Agora aguarda restante da resposta
+        // ----------------------------------------------------------
+
+        timer.setTimeout(ms(TMAXCAR_MSEC));
+        while (!timer.timedOut() || !txTimer.timedOut()) {
+            size_t read = _porta->read(&resposta[respostaIndex],
+                                       resposta.size() - respostaIndex);
+            respostaIndex += read;
+
+            if (read)
+                timer.setTimeout(
+                    ms(TMAXCAR_MSEC)); // recarrega timeout para
+                                       // receber próximo(s) byte(s)
+            if (respostaIndex < resposta.size())
+                continue;
+
+            // ------------------------------------
+            // resposta completa recebida. Trata-a.
+            // ------------------------------------
+
+            uint16_t crcRecv = getCRC(resposta);
+            uint16_t crcCalc = CRC16(resposta.data(), resposta.size() - 2);
+
+            if (crcRecv != crcCalc) {
+                // se CRC com erro, o medidor deve enviar um NAK para o
+                // leitor e aguardar novo COMANDO. O máximo de NAKs
+                // enviados para um mesmo comando é 7.
+
+                if (countTxNAK >= MAX_BLOCO_NAK)
+                    return ERR_RSP_NAK_TX_EXCEDIDO;
+
+                byte_t sinalizador = NAK;
+                _porta->write(&sinalizador, 1);
+
+                countTxNAK++;
+
+                timer.setTimeout(ms(TMAXRSP_MSEC));
+                continue;
+            }
+
+            // -----------------------------------
+            // resposta completa recebida está OK!
+            // -----------------------------------
+
+            byte_t sinalizador = ACK;
+            _porta->write(&sinalizador, 1);
+            // TODO (maybe): talvez tenha q add um tempo aqui pra ter certeza
+            // que o medidor recebeu o ACK
+
+            cb_rsp_received(resposta);
+
+            if (isComposedCodeCommand(resposta.at(0))) {
+                // os unicos 3 comandos compostos existentes na norma (0x26,
+                // 0x27 e 0x52) possuem o octeto 006 (5o byte), cujo valor:
+                // 0N -> resposta/bloco intermediário
+                // 1N -> resposta/bloco final
+
+                if (resposta.at(5) & 0x10) {
+                    // resposta final do comando composto
+                    return RSP_RECEBIDA;
+                } else {
+                    // resposta intermediária, aguarda próxima resposta
+
+                    // recarrega timeout da leitura
+                    txTimer.setTimeout(timeout);
+                    goto waitFirstByte;
+                }
+
+            } else {
+                return RSP_RECEBIDA;
+            }
+        }
+
+        return ERR_RSP_TOTAL_TEMPO_EXCEDIDO;
     }
 };
 
--- tests/leitor.cpp
+++ tests/leitor.cpp
@@ -9,41 +9,52 @@
 #include <thread>
 
 using namespace NBR14522;
+using namespace std::literals;
 
+using MedidorSimulado = Simulador::Medidor<>;
+
+// TEST_CASE("leitor com medidor simulado" * doctest::skip()) {
 TEST_CASE("leitor com medidor simulado") {
+    medidor_num_serie_t serie = {5, 6, 7, 8};
+    std::shared_ptr<MedidorSimulado> medidor =
+        std::make_shared<MedidorSimulado>(serie);
 
-    std::shared_ptr<Simulador::Medidor> medidor =
-        std::make_shared<Simulador::Medidor>();
-
-    Leitor leitor(medidor);
+    Leitor<> leitor(medidor);
     std::vector<resposta_t> respostas;
     comando_t comando;
-    Leitor::estado_t estado;
+    Leitor<>::estado_t estado;
 
     // timeout deve acontecer, pois medidor nao foi posto para executar ainda
-    estado = leitor.tx(
+    estado = leitor.read(
         comando, [](const resposta_t& rsp) {}, 100ms);
-    CHECK(estado == Leitor::SINCRONIZACAO_FALHOU);
+    REQUIRE(estado == Leitor<>::ERR_SINC_TEMPO_EXCEDIDO);
 
     // coloca medidor para executar
-    std::thread t1(&Simulador::Medidor::run, medidor);
+    std::thread t1(&MedidorSimulado::run, medidor);
+    std::this_thread::sleep_for(5ms);
+
+    Timer<> timer;
+
+    // verifica se ENQ está sendo enviado pelo medidor
+    timer.setTimeout(std::chrono::milliseconds(2 * TMAXSINC_MSEC));
+    byte_t enq = 0;
+    while (!timer.timedOut()) {
+        if (medidor->read(&enq, 1) == 1)
+            break;
+    }
+    REQUIRE(enq == ENQ);
+    REQUIRE(!timer.timedOut());
 
     SUBCASE("0x14") {
 
         comando.at(0) = 0x14;
-        estado = leitor.tx(
-            comando,
-            [&](const resposta_t& rsp) {
-                CHECK(1 == 0);
-                respostas.push_back(rsp);
-                //
-            },
-            2000ms);
+        estado = leitor.read(
+            comando, [&](const resposta_t& rsp) { respostas.push_back(rsp); },
+            1000ms);
 
-        CHECK(estado == Leitor::RESPOSTA_RECEBIDA);
-
-        // CHECK(respostas.size() == 1);
-        // CHECK(respostas.at(0).at(0) == 0x14);
+        CHECK(estado == Leitor<>::RSP_RECEBIDA);
+        CHECK(respostas.size() == 1);
+        CHECK(respostas.at(0).at(0) == 0x14);
     }
 
     medidor->runStop();
