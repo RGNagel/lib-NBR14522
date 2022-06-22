2d1279339e13e56bdd023ecee822e08b3a19286c
Rafael Nagel <rafael.gustavo.nagel@gmail.com>
2022-06-22 11:36:04 -0300

    leitor-cli add usage; c++ v. 14

--- app/leitor/leitor-cli.cpp
+++ app/leitor/leitor-cli.cpp
@@ -13,7 +13,27 @@ void print_arr_hex(const std::array<T, N>& arr) {
 }
 
 // TODO
-void print_usage() {}
+void print_usage() {
+    printf(
+        "Este programa recebe dois argumentos:\n\n"
+        "1 - porta serial que o medidor está conectado. Em sistemas unix será\n" 
+        "um pseudo-arquivo localizado em /dev/, por exemplo: /dev/ttyUSB0,\n"
+        "/dev/ttyUSB1, etc. No Windows: COM1, COM2, etc\n\n"
+
+        "2 - comando da comunicação convencional leitor-medidor (NBR14522). \n"
+        "Este programa interpreta este parâmetro na codificação BCD. Caso o \n"
+        "comando não seja passado por parâmetro contendo todos os 64 bytes \n"
+        "referentes ao código + payload, o programa preenche o restante dos \n"
+        "bytes com zeros. Os dois últimos bytes referentes ao CRC são sempre \n"
+        "adicionados ou sobrescritos pela biblioteca NBR14522.\n\n"
+
+        "Exemplos de invocação do programa:\n"
+        "./leitor-cli /dev/ttyUSB0 14\n"
+        "./leitor-cli /dev/ttyUSB0 14123456\n"
+        "./leitor-cli /dev/ttyUSB0 20\n"
+        "./leitor-cli /dev/ttyUSB0 204455660101\n\n"
+    );
+}
 
 std::vector<byte_t> get_hex_bytes(const std::string& hexbytes) {
     std::vector<byte_t> retval;
@@ -33,7 +53,7 @@ std::vector<byte_t> get_hex_bytes(const std::string& hexbytes) {
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
-        printf("Invalid number of args\n");
+        printf("Número de argumentos inválido.\n\n");
         print_usage();
         return EXIT_FAILURE;
     }
@@ -42,7 +62,7 @@ int main(int argc, char* argv[]) {
         std::make_shared<SerialPolicyWinUnix>();
 
     if (!porta->open(argv[1], 9600)) {
-        printf("Não foi possível abrir a porta serial\n");
+        printf("Não foi possível abrir a porta serial\n\n");
         return EXIT_FAILURE;
     }
 
@@ -58,6 +78,7 @@ int main(int argc, char* argv[]) {
     }
 
     for (auto& comando : comandos) {
+        setCRC(comando, CRC16(comando.data(), comando.size()-2));
         printf("\nComando:\n");
         print_arr_hex(comando);
 
